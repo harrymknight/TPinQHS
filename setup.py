5626c612429de91b33c9fd97a986c6cd4edc4749
Harry Knight <harry.knight@hotmail.co.uk>
2020-07-09 20:15:32 +0100

    Include directory to numpy headers

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, Extension
+import numpy as np
 
 wavefunc_ext = Extension(
     name="wavefunc",
-    sources=["ode.c", "brent.c", "Pywavefunc.c"])
+    sources=["ode.c", "brent.c", "pywavefunc.c"],
+    include_dirs=[np.get_include()] 
+)
 
 def main():
     setup(name="wavefunc",
@@ -10,7 +13,7 @@ def main():
           description="Python interface for routines which solve for properties of wavefunctions",
           author="harry knight",
           author_email="harry.knight@hotmail.co.uk",
-          ext_modules=wavefunc_ext)
+          ext_modules=[wavefunc_ext])
 
 if __name__ == "__main__":
     main()
